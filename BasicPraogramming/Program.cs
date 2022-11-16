16bfa10b109244cdebdd1474012b507937f6fb7e
Ankit <ankitcool4370@gmail.com>
2022-11-16 13:29:47 +0530

    UC1-SwappingNumber

--- BasicPraogramming/Program.cs
+++ BasicPraogramming/Program.cs
@@ -10,30 +10,15 @@ namespace BasicPraogramming
     {
         static void Main(string[] args)
         {
-            int heads = 0;
-            int tails = 0;
-            int counter = 0;
-            Random coinflip = new Random();
-
-            Console.WriteLine("How many times would you like to flip a coin? ");
-            counter = Convert.ToInt32(Console.ReadLine());
-
-            for (int i = 0; i < counter; i++)
-            {
-                int flip = coinflip.Next(1, 3);
-                if (flip == 1)
-                {
-                    heads++;
-                }
-                else
-                {
-                    tails++;
-                }
-            }
-
-            Console.WriteLine("You flipped a coin : " + counter
-               + "times " + "and you got " + heads + "heads and " + tails + "tails.");
-            Console.WriteLine();
+            Console.WriteLine("Enter A: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter B: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int temp;
+            temp = a;
+            Console.WriteLine("Swapped Value of A= " + b);
+            Console.ReadLine();
+            Console.WriteLine("Swapped value of B= " + a);
             Console.ReadLine();
         }
         
