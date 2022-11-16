c12a2c7ae7b6d3255ad10995705dbe4a314ff3d6
Ankit <ankitcool4370@gmail.com>
2022-11-16 14:43:11 +0530

    HormanicNumber

--- BasicPraogramming/Program.cs
+++ BasicPraogramming/Program.cs
@@ -11,32 +11,21 @@ namespace BasicPraogramming
         static void Main(string[] args)
         {
 
-            int num1, num2, num3;
+            int i, n;
+            double s = 0.0;
+
             
-            Console.Write("Find the largest of three numbers:\n");
+            Console.WriteLine("Calculate the harmonic series and their sum:\n");
 
-            Console.Write("Input the 1st number :");
-            num1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Input the  2nd number :");
-            num2 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Input the 3rd  number :");
-            num3 = Convert.ToInt32(Console.ReadLine());
-
-            if (num1 > num2)
+            Console.Write("Input the number of terms : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            for (i = 1; i <= n; i++)
             {
-                if (num1 > num3)
-                {
-                    Console.Write("The 1st Number is the greatest among three. \n\n");
-                }
-                else
-                {
-                    Console.Write("The 3rd Number is the greatest among three. \n\n");
-                }
+                Console.WriteLine("1/{0} + ", i);
+                s += 1 / (float)i;
             }
-            else if (num2 > num3)
-                Console.Write("The 2nd Number is the greatest among three \n\n");
-            else
-                Console.Write("The 3rd Number is the greatest among three \n\n");
+            Console.WriteLine("\nSum of Series upto {0} terms : {1} ", n, s);
             Console.ReadLine();
         }
        
